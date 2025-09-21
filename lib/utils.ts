ccd79160521fdd50f85bc04a6a675c3acce4a758
eniola <eniolafaithful57@gmail.com>
2025-09-22 00:10:05 +0100

    feat: add environment variable exports for email service configuration

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export const template_id=process.env.TEMPLATE_ID;
+export const service_id=process.env.SERVICE_ID;
+export const public_key=process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
